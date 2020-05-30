c56ce90e83704f8e9099f0564e1ca6e4a9b03410
Yussel Paredes <yussel_paredes@hotmail.com>
2020-05-30 11:51:57 -0500

    test: add giphy service tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'frontend-test app is running!'
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain(
+      'Frontend Test'
     );
   });
 });
