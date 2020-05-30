4de2b887060d7b7267122a28a3aff3c60138027b
Yussel Paredes <yussel_paredes@hotmail.com>
2020-05-30 16:05:54 -0500

    test: add search bar tests

--- src/app/shared/search-bar/search-bar.component.spec.ts
+++ src/app/shared/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { SearchBarComponent } from './search-bar.component';
+import { FormsModule } from '@angular/forms';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -8,7 +10,8 @@ describe('SearchBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchBarComponent]
+      declarations: [SearchBarComponent],
+      imports: [FormsModule]
     }).compileComponents();
   }));
 
@@ -20,5 +23,25 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a search input', () => {
+    expect(
+      fixture.debugElement.query(By.css('input[type="search"]'))
+    ).toBeTruthy();
+  });
+
+  it('should emit queryChange event on input text changes', () => {
+    const searchPhrase = 'search phrase';
+    spyOn(component.queryChange, 'emit');
+
+    const inputEle = fixture.debugElement.query(By.css('input[type="search"]'))
+      .nativeElement;
+    inputEle.value = searchPhrase;
+    inputEle.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(component.queryChange.emit).toHaveBeenCalledWith(searchPhrase);
   });
 });
